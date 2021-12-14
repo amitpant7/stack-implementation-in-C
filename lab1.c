b380f800a4f4516d13aa6c27588965d8af322ad5
Amit Pant <50907565+amitpant7@users.noreply.github.com>
2021-12-14 19:11:32 +0545

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+int maxsize = 5;
+int top=-1;
+void push(int *);
+void pop(int *);
+void display(int *);
+void displaytop(int *);
+
+int main()
+{
+    int stack[5];
+    int end= 0;
+    printf("Implementation of Stack Using C \n \n");
+    while(end == 0){   
+    int input;
+    printf("Select Any Option:\n");
+    printf("[1]. Push \n");
+    printf("[2]. Pop \n");
+    printf("[3]. Display All \n");
+    printf("[4]. Display TOS \n");
+    printf("[5]. Exit \n");
+    printf("Enter the Option: ");
+    scanf("%d", &input);
+
+    switch(input)
+    {
+    case 1:
+        push(&stack[0]);
+		break;
+    case 2:
+    	pop(&stack[0]);
+    	break;
+    	
+    case 3:
+    	display(&stack[0]);
+    	break;
+    case 4:
+    	displaytop(&stack[0]);
+    	break; 
+    case 5:
+    	end= 1;
+		break; 
+	}
+	printf("----------------------------------------------\n");
+}
+	
+	return 0;
+
+}
+
+
+void push(int *s)
+{
+    if (top== maxsize)
+    {
+     printf("Stack Overflow\n");
+    } 
+    if (top < maxsize)
+     {
+     	top++;
+    	int x;
+        printf("Enter the Element to add:");
+        scanf("%d", &x);
+        *(s+top)=x;
+    //    printf("the num is :%d", *(s+top));
+    //    printf("/n Top=%d", top);
+
+      }
+}
+
+void pop (int *s)
+{
+	if (top == -1)
+		printf("Stack Underflow");
+	else 
+	{
+		printf("The element poped: %d\n", *(s+top));
+		top--;
+	}
+	
+}
+
+void display(int *s)
+{
+	int i;
+		printf("The elements are :\n");
+	for (i=0; i<=top; i++)
+	{
+		printf("%d \n", *(s+i));	
+	}
+}
+
+void displaytop(int *s)
+{
+		printf(" The top of Stack: %d\n", *(s+top));
+}
+
+
+
